0833c7675b1ac27c2191952d99c58dd0c62bbf47
iamtheceo <137496207+vityarampage@users.noreply.github.com>
2025-03-13 14:21:28 +0300

    Create config.py
    v 1.0

---
+++ config.py
@@ -0,0 +1,9 @@
+BINANCE_API_KEY = 'your_binance_api_key'
+BINANCE_API_SECRET = 'your_binance_api_secret'
+COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+NICEHASH_API_KEY = 'your_nicehash_api_key'
+NICEHASH_USER_ID = 'your_nicehash_user_id'
+
+# Mining parameters
+CRYPTO_ID = 'bitcoin'  # Cryptocurrency to mine
+STABLECOIN_SYMBOL = 'BTCUSDT'  # Cryptocurrency to exchange (BTC -> USDT)
